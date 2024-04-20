e6e9a8723ba533c725f2bfff4c294add2c764eb1
Savaira <savaira.talaat@gmail.com>
2024-04-20 17:28:48 +0200

    modif

--- detailsHtml.php
+++ detailsHtml.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Accueil</title>
-    <link rel="stylesheet" href="details.css" />
+    <link rel="stylesheet" href="Css/details.css" />
     <link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css"/>
   </head>
   <body>
