a74012fe5ce74d6ea7c1aacd389208be4b0edefb
Savaira <savaira.talaat@gmail.com>
2024-04-21 20:12:57 +0200

    modif fichier bdd

--- bdd/my_base3.sql
+++ bdd/my_base3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : sam. 20 avr. 2024 à 18:33
+-- Généré le : dim. 21 avr. 2024 à 18:11
 -- Version du serveur : 8.2.0
 -- Version de PHP : 8.2.13
 
@@ -24,6 +24,23 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `cart`
+--
+
+DROP TABLE IF EXISTS `cart`;
+CREATE TABLE IF NOT EXISTS `cart` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `poster` varchar(255) DEFAULT NULL,
+  `titre` varchar(255) DEFAULT NULL,
+  `note` float DEFAULT NULL,
+  `overview` varchar(255) DEFAULT NULL,
+  `movieid` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `users`
 --
 
@@ -33,14 +50,16 @@ CREATE TABLE IF NOT EXISTS `users` (
   `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Déchargement des données de la table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(7, 'Hinata', '$2y$10$g53mvtkBJ6w1WSmI/7gp0uBBEz.ON8VIWzT8c4jjbRJel8N6gcJuW');
+(10, 'Hinatacos', '$2y$10$YF0c77sJLaIMidqoma2HjeKFz8Vf7eog7Ve/Me7dNSqQPH.oelH/a'),
+(7, 'Hinata', '$2y$10$g53mvtkBJ6w1WSmI/7gp0uBBEz.ON8VIWzT8c4jjbRJel8N6gcJuW'),
+(11, 'Hinatacos3viandes', '$2y$10$kwQs1mLqRfTAHOvODz6B0.Pq8ymx3Uk1xR3Xqu/o4G3QkH3ZkKM6.');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
